489ed292afe364b40f6a79ceeaad720151c910bb
Alex Chiew <alex.chiewyz@gmail.com>
2023-05-09 00:36:54 +0800

    Add resolvers for GraphQL API queries, mutations and types in index.js in resolvers directory

---
+++ src/graphql/resolvers/index.js
@@ -0,0 +1,11 @@
+import authResolvers from './Auth';
+import paramResolvers from './Params';
+import userResolvers from './Users';
+import noteResolvers from './Notes';
+
+export default [
+    authResolvers,
+    paramResolvers,
+    userResolvers,
+    noteResolvers
+];
