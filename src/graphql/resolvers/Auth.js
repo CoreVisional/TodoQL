fce395ef1b4ce92832ec299dd4723ab6b46d6caa
Alex Chiew <alex.chiewyz@gmail.com>
2023-05-09 00:34:28 +0800

    Add user authentication and registration resolvers

---
+++ src/graphql/resolvers/Auth.js
@@ -0,0 +1,48 @@
+import { User } from "../../models";
+import { hashPassword, authenticate, generateJWT } from "../../utils/security";
+import { GraphQLError } from "graphql";
+
+const resolvers = {
+    Mutation: {
+        async register(root, args, context) {
+            const { email, password } = args.input;
+
+            // Hash the password before creating the user
+            const hashedPassword = await hashPassword(password);
+
+            const newUser = await User.create({
+                email,
+                password: hashedPassword,
+            });
+
+            return newUser;
+        },
+
+        async login(root, args, { res }) {
+            const { email, password } = args.input;
+
+            const user = await User.scope(null).findOne({ where: { email } });
+
+            if (user) {
+                const isPasswordValid = await authenticate(
+                    password,
+                    user.password
+                );
+
+                if (isPasswordValid) {
+                    const token = generateJWT(user);
+                    return { ...user.toJSON(), token };
+                }
+            }
+
+            throw new GraphQLError("Invalid credentials", {
+                extensions: {
+                    code: "UNAUTHENTICATED",
+                    http: { status: 401 },
+                },
+            });
+        },
+    },
+};
+
+export default resolvers;
