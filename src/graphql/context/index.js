e565840e3bb2a4cb2534760ee1730b20c0c2b603
Alex Chiew <alex.chiewyz@gmail.com>
2023-05-09 00:38:08 +0800

    Add verifyToken function to handle authentication in GraphQL context

---
+++ src/graphql/context/index.js
@@ -0,0 +1,27 @@
+import { User } from "../../models";
+import jwt from "jsonwebtoken";
+import { secret } from "../../../config/jwt";
+
+const verifyToken = async (token) => {
+    try {
+        if (!token) {
+            return null;
+        }
+        const decodedToken = await jwt.verify(token, secret);
+        const user = await User.findOne({ where: { id: decodedToken.userid } });
+        return user;
+    } catch (error) {
+        if (error instanceof jwt.JsonWebTokenError) {
+            throw new GraphQLError("Invalid or Expired token", {
+                extensions: {
+                    code: "UNAUTHENTICATED",
+                    http: { status: 401 },
+                },
+            });
+        } else {
+            throw new Error("Unknown error during token verification");
+        }
+    }
+};
+
+export default verifyToken;
