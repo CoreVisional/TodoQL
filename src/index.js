2a910a86da055d07dc9c9bce280b632bcea0177d
Alex Chiew <alex.chiewyz@gmail.com>
2023-05-09 10:09:53 +0800

    Update Apollo Server configuration to use express middleware and plugins and change from using token from headers to storing it in cookie

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 import { port } from "../config/index";
 import typeDefs from "./graphql/schemas";
 import resolvers from "./graphql/resolvers";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { makeExecutableSchema } from "@graphql-tools/schema";
+import express from "express";
+import cors from "cors";
+import http from "http";
+import { json } from "body-parser";
 import verifyToken from "./graphql/context";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+const httpServer = http.createServer(app);
 
 const schema = makeExecutableSchema({
     typeDefs,
@@ -16,27 +26,47 @@ const server = new ApolloServer({
     csrfPrevention: true,
     introspection: true,
     status400ForVariableCoercionErrors: true,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
+
+app.use(cookieParser());
 
 const startServer = async () => {
     try {
-        const { url } = await startStandaloneServer(server, {
-            listen: { port: port },
-            context: async ({ req, res }) => {
-                const authHeader = req.headers.authorization || "";
+        await server.start();
 
-                // Check for Bearer token
-                if (authHeader && authHeader.startsWith("Bearer ")) {
-                    const token = authHeader.split(" ")[1];
-                    const user = await verifyToken(token);
-                    return { res, user };
-                }
+        app.use(
+            cors({
+                origin: "*",
+                credentials: true,
+            }),
+            json(),
+            expressMiddleware(server, {
+                context: async ({ req, res }) => {
+                    // Get the JWT token from the cookie
+                    const token = req.cookies.jwt;
 
-                return { res };
-            },
+                    // Verify the JWT token and obtain the user information
+                    let user = null;
+                    if (token) {
+                        try {
+                            user = await verifyToken(token);
+                        } catch (err) {
+                            console.error("Error verifying token:", err);
+                        }
+                    }
+
+                    // Return the context object with the request, response, and user
+                    return { req, res, user };
+                },
+            })
+        );
+
+        await new Promise((resolve) => {
+            httpServer.listen({ port: port }, resolve);
         });
 
-        console.log(`${"🚀 Server ready at:"} ${url}api`);
+        console.log(`🚀 Server ready at http://localhost:${port}/graphql`);
     } catch (error) {
         console.log(error);
     }
